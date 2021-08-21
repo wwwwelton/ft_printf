4ba628b62b97e93e047e9cf9436cbc0d0a6fae7a
wleite <wleite@student.42sp.org.br>
2021-08-21 01:57:39 -0300

    refactor: move str_replace to utils file

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 06:02:58 by wleite            #+#    #+#             */
-/*   Updated: 2021/08/20 19:35:54 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/21 01:56:45 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,42 +60,4 @@ int	ft_printf(const char *format, ...)
 	printf("%s", p);
 	free(p);
 	return (len);
-}
-
-char	*str_replace(char *str, char *old_word, char *new_word)
-{
-	char	*result;
-	char	*result_ptr;
-	size_t	old_word_len;
-	size_t	new_word_len;
-	size_t	str_len;
-	size_t	replaced;
-
-	old_word_len = ft_strlen(old_word);
-	new_word_len = ft_strlen(new_word);
-	str_len = ft_strlen(str);
-	result_ptr = (char *)malloc((str_len - old_word_len + new_word_len) + 1);
-	if (!result_ptr)
-		return (NULL);
-	*result_ptr = '\0';
-	result = result_ptr;
-	replaced = 0;
-	while (*str)
-	{
-		if (!ft_strncmp (str, old_word, old_word_len) && !replaced)
-		{
-			ft_strlcat (result, new_word, new_word_len + 1);
-			str += old_word_len;
-			result += new_word_len;
-			replaced = 1;
-		}
-		else
-		{
-			*result = *str;
-			result++;
-			str++;
-		}
-		*result = '\0';
-	}
-	return (result_ptr);
 }
---
+++ ft_printf_utils.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/21 01:53:59 by wleite            #+#    #+#             */
+/*   Updated: 2021/08/21 01:54:21 by wleite           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char	*str_replace(char *str, char *old_word, char *new_word)
+{
+	char	*result;
+	char	*result_start;
+	size_t	replaced;
+
+	result = (char *)ft_calloc((ft_strlen(str) - ft_strlen(old_word)
+		+ ft_strlen(new_word) + 1), sizeof(char *));
+	result_start = result;
+	replaced = 0;
+	while (*str)
+	{
+		if (!ft_strncmp (str, old_word, ft_strlen(old_word)) && !replaced)
+		{
+			ft_strlcat (result, new_word, ft_strlen(new_word) + 1);
+			str += ft_strlen(old_word);
+			result += ft_strlen(new_word);
+			replaced = 1;
+		}
+		else
+		{
+			*result = *str;
+			result++;
+			str++;
+		}
+	}
+	return (result_start);
+}
