83b5e0a73fe4d9ad68a4db06d7543df436950342
wleite <wleite@student.42sp.org.br>
2021-08-20 21:25:15 -0300

    feat: include helper functions

--- ft_printf.h
+++ ft_printf.h
@@ -1,8 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/19 06:03:07 by wleite            #+#    #+#             */
+/*   Updated: 2021/08/20 19:14:44 by wleite           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdarg.h>
+# include "libft.h"
+# include <stdarg.h>
+# include <stdio.h>
+# include <string.h>
 
-int	ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
+char	*str_replace (char *str, char *old_word, char *new_word);
 
 #endif
