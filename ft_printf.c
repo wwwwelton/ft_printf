68466992649d7b991fc1c630797c034f93bd6e82
wleite <wleite@student.42sp.org.br>
2021-08-22 01:22:02 -0300

    refactor: change index to pointer arithmetic

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 06:02:58 by wleite            #+#    #+#             */
-/*   Updated: 2021/08/21 16:43:11 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/21 23:59:10 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,51 @@ int	ft_printf(const char *format, ...)
 	va_list	ap;
 	char	*fmt;
 	int		fmt_len;
-	int		i;
 
 	va_start(ap, format);
 	fmt = ft_strdup(format);
-	i = 0;
-	while (fmt[i] != '\0')
+	while (*format)
 	{
-		if (is_decimal(&fmt[i]))
-			fmt = replace_decimal(fmt, va_arg(ap, int));
-		else if (is_string(&fmt[i]))
-			fmt = replace_string(fmt, va_arg(ap, char *));
-		else if (is_char(&fmt[i]))
+		if (is_char(format))
 			fmt = replace_char(fmt, va_arg(ap, int));
-		else
-			i++;
+		else if (is_string(format))
+			fmt = replace_string(fmt, va_arg(ap, char *));
+		else if (is_decimal(format))
+			fmt = replace_decimal(fmt, va_arg(ap, int));
+		format++;
 	}
 	va_end(ap);
 	fmt_len = ft_strlen(fmt);
-	printf("%s", fmt);
+	write(1, fmt, fmt_len);
 	free_ptr(&fmt);
 	return (fmt_len);
 }
+
+/*
+while (fmt[i] != '\0')
+	{
+		//c
+		if (is_char(&fmt[i]))
+			fmt = replace_char(fmt, va_arg(ap, int));
+		//s
+		else if (is_string(&fmt[i]))
+			fmt = replace_string(fmt, va_arg(ap, char *));
+		//p
+		// else if (is_pointer(&fmt[i]))
+		// 	fmt = replace_pointer(fmt, va_arg(ap, int));
+		//d | i
+		else if (is_decimal(&fmt[i]))
+			fmt = replace_decimal(fmt, va_arg(ap, int));
+		// //u
+		// else if (is_unsigned_int(&fmt[i]))
+		// 	fmt = replace_unsigned_int(fmt, va_arg(ap, int));
+		// //x | X
+		// else if (is_hexa_decimal(&fmt[i]))
+		// 	fmt = replace_hexa_decimal(fmt, va_arg(ap, int));
+		// //%%
+		// else if (is_mod(&fmt[i]))
+		// 	fmt = replace_mod(fmt, va_arg(ap, int));
+		else
+			i++;
+	}
+*/
