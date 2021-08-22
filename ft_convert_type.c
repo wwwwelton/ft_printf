3514e09d8f75228a7ae8b13497b898f4d83c7bf0
wleite <wleite@student.42sp.org.br>
2021-08-22 02:43:58 -0300

    refactor: adds convert_type to replace flags

--- ft_convert_type.c
+++ ft_convert_type.c
@@ -1,28 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_convert_type.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/19 06:02:58 by wleite            #+#    #+#             */
-/*   Updated: 2021/08/22 02:33:17 by wleite           ###   ########.fr       */
+/*   Created: 2021/08/22 02:24:09 by wleite            #+#    #+#             */
+/*   Updated: 2021/08/22 02:34:09 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *format, ...)
+char	*ft_convert_type(const char *format, va_list *ap)
 {
-	va_list	ap;
 	char	*fmt;
-	int		fmt_len;
 
-	va_start(ap, format);
-	fmt = ft_convert_type(format, &ap);
-	va_end(ap);
-	fmt_len = ft_strlen(fmt);
-	write(1, fmt, fmt_len);
-	free_ptr(&fmt);
-	return (fmt_len);
+	fmt = ft_strdup(format);
+	while (*format)
+	{
+		if (is_char(format))
+			fmt = replace_char(fmt, va_arg(*ap, int));
+		else if (is_string(format))
+			fmt = replace_string(fmt, va_arg(*ap, char *));
+		else if (is_decimal(format))
+			fmt = replace_decimal(fmt, va_arg(*ap, int));
+		else if (is_percent(format))
+			fmt = replace_percent(fmt);
+		format++;
+	}
+	return (fmt);
 }
