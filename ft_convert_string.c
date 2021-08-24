cf47dd70e582685af165240539eda05feb8670dd
wleite <wleite@student.42sp.org.br>
2021-08-24 03:42:57 -0300

    fix: replace null value with (null) word

--- ft_convert_string.c
+++ ft_convert_string.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 02:11:13 by wleite            #+#    #+#             */
-/*   Updated: 2021/08/22 14:55:34 by wleite           ###   ########.fr       */
+/*   Updated: 2021/08/24 03:41:37 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,10 @@ char	*replace_string(char *format, char *value)
 	char	*old_adress;
 	char	*string;
 
-	string = ft_strdup(value);
+	if (!value)
+		string = ft_strdup("(null)");
+	else
+		string = ft_strdup(value);
 	old_adress = format;
 	format = str_replace(old_adress, "%s", string);
 	free_ptr(&string);
