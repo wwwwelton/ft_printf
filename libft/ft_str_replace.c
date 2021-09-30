b75df8e5584e0fe68c7d416c11f86f8e0ef23a31
wleite <wleite@student.42sp.org.br>
2021-09-29 22:05:54 -0300

    chore: updated libft

--- libft/ft_str_replace.c
+++ libft/ft_str_replace.c
@@ -6,25 +6,28 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 17:03:55 by wleite            #+#    #+#             */
-/*   Updated: 2021/09/28 18:15:09 by wleite           ###   ########.fr       */
+/*   Updated: 2021/09/29 22:01:27 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_str_replace(const char *str,
-	const char *old_word, const char *new_word)
+char	*ft_str_replace(char *str,	const char *old_word, const char *new_word)
 {
-	char	*new_str;
-	char	*ptr;
-	int		total_size;
 	int		i;
+	int		total_size;
+	char	*ptr;
+	char	*new_str;
 
 	if (!str || !old_word || !new_word)
 		return (NULL);
 	ptr = ft_strnstr(str, old_word, ft_strlen(str));
 	if (!ptr)
-		return (ft_strdup(str));
+	{
+		new_str = ft_strdup(str);
+		ft_free_ptr((void *)&str);
+		return (new_str);
+	}
 	total_size = ft_strlen(str) - ft_strlen(old_word) + ft_strlen(new_word) + 1;
 	new_str = (char *)malloc(sizeof(char) * total_size);
 	if (!new_str)
@@ -33,5 +36,6 @@ char	*ft_str_replace(const char *str,
 	ft_strlcat(new_str, new_word, total_size);
 	i = (ptr - str) + ft_strlen(old_word);
 	ft_strlcat(new_str, &str[i], total_size);
+	ft_free_ptr((void *)&str);
 	return (new_str);
 }
