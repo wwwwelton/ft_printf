8bd17973df2d41be18272dc02022e98da54af649
wleite <wleite@student.42sp.org.br>
2021-10-13 00:48:32 -0300

    docs: updated ft_printf description

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/19 06:03:07 by wleite            #+#    #+#             */
-/*   Updated: 2021/10/12 12:28:40 by wleite           ###   ########.fr       */
+/*   Updated: 2021/10/13 00:33:16 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,17 +59,13 @@ void	init_args(t_args *args);
 /**
  * @brief Sends formatted output to stdout.
  *
- * @param format This is the string that contains the text to be written
- * to stdout. It can optionally contain embedded format tags
- * that are replaced by the values specified in subsequent
- * additional arguments and formatted as requested. Format
- * tags prototype is %[flags][width][.precision][length]specifier.
- * @param ... Depending on the format string, the function
- * may expect a sequence of additional arguments, each
- * containing one value to be inserted instead of each
- * %-tag specified in the format parameter (if any).
- * There should be the same number of these arguments
- * as the number of %-tags that expect a value.
+ * @param format This is the null-terminated string (C-string)
+ * that is written to stdout. It consists of characters along with
+ * optional format specifiers starting with %. Format tags prototype is
+ * %[flags][width][.precision]specifier.
+ * @param ... Depending on the format string, other additional
+ * arguments specifying the data to be printed.
+ * They occur in a sequence according to the format specifier.
  * @return The total number of characters written is returned.
  */
 int		ft_printf(const char *format, ...);
